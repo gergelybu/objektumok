b00c7fb83f37e1cca116b4e4a6c53b225846cc28
Burai Gergely (SZF_F_2022_2024) <burai.gergely@diak.szamalk-szalezi.hu>
2023-03-18 08:48:23 +0100

    Gomb és funkció hozzáadása

---
+++ main.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", init);
+
+function init() {
+  const KUTYALISTA = [
+    "foxterrier",
+    "kuvasz",
+    "puli",
+    "agár",
+    "újfullandi",
+    "keverék",
+  ];
+  const ARTICLE = document.querySelector("article");
+  ARTICLE.innerHTML = "<button>kutya</button>";
+  const GOMB = document.querySelector("button");
+  GOMB.addEventListener("click", function () {
+    kutya(KUTYALISTA, ARTICLE);
+  });
+}
+
+function kutya(LISTA, ARTICLE) {
+  ARTICLE.innerHTML += LISTA[2];
+}
