dce942d2b4811d92da7f8e96c20e3862b4830db6
Burai Gergely (SZF_F_2022_2024) <burai.gergely@diak.szamalk-szalezi.hu>
2023-03-18 11:13:22 +0100

    Feladatok megoldása

--- main.js
+++ main.js
@@ -1,23 +1,94 @@
 window.addEventListener("load", init);
 
+const KUTYALISTA = [
+  {
+    nev: "Dézi",
+    fajta: "keverék",
+    marmagassag: 52,
+    neme: "szuka",
+    kor: 13,
+  },
+  {
+    nev: "Kira",
+    fajta: "yorkshire terrier",
+    marmagassag: 20,
+    neme: "szuka",
+    kor: 13,
+  },
+  {
+    nev: "Bobo",
+    fajta: "shiba inu",
+    marmagassag: 40,
+    neme: "kan",
+    kor: 5,
+  },
+  {
+    nev: "Kutya",
+    fajta: "golden retriever",
+    marmagassag: 60,
+    neme: "kan",
+    kor: 9,
+  },
+];
+
 function init() {
-  const KUTYALISTA = [
-    "foxterrier",
-    "kuvasz",
-    "puli",
-    "agár",
-    "újfullandi",
-    "keverék",
-  ];
   const ARTICLE = document.querySelector("article");
-  ARTICLE.innerHTML = "<button>kutya</button>";
-  const GOMB = document.querySelector("button");
-  GOMB.addEventListener("click", function () {
+  const MAIN = document.querySelector("main");
+  console.log(KUTYALISTA);
+  let txt = osszeAllit();
+  ARTICLE.innerHTML = txt;
+  txt = tablazat();
+  MAIN.innerHTML = txt;
+  const GOMB = document.getElementsByClassName("torles");
+  for (let index = 0; index < GOMB.length; index++) {
+    GOMB[index].addEventListener("click", function () {
+      torles(index);
+    });
+  }
+
+  /*GOMB.addEventListener("click", function () {
     kutya(KUTYALISTA, ARTICLE);
-  });
+  })*/
 }
 
-
-function kutya(LISTA, ARTICLE) {
+/*function kutya(LISTA, ARTICLE) {
   ARTICLE.innerHTML += LISTA[2];
+}*/
+
+function osszeAllit() {
+  let txt = "";
+  for (let index = 0; index < KUTYALISTA.length; index++) {
+    txt += "<div>";
+    for (const key in KUTYALISTA[index]) {
+      txt += `<p>
+                     ${key}: ${KUTYALISTA[index][key]}
+                 </p>`;
+      //console.log(KUTYALISTA[index][key])
+    }
+    txt += "<button class='torles'>Törlés</button></div>";
+  }
+  //console.log(txt);
+  return txt;
+}
+
+function tablazat() {
+  let txt = "<table>";
+  for (const key in KUTYALISTA[0]) {
+    txt += `<th>${key}</th>`;
+  }
+  for (let index = 0; index < KUTYALISTA.length; index++) {
+    txt += "<tr>";
+    for (const key in KUTYALISTA[index]) {
+      txt += `<td>${KUTYALISTA[index][key]}</td>`;
+    }
+    txt += "</tr>";
+  }
+  txt += "</thead>";
+  return txt;
+}
+
+function torles(i) {
+  console.log(KUTYALISTA[i]);
+  KUTYALISTA.splice(i);
+  console.log(KUTYALISTA[i]);
 }
