a203d8c420921ac8feaac847f6df9fb7e75c809e
gergelybu <118608660+gergelybu@users.noreply.github.com>
2023-03-20 15:08:26 +0100

    Űrlappal adatok feltöltés + törlés javítása

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const KUTYALISTA = [
     kor: 13,
   },
   {
-    nev: "Bobo",
+    nev: "Doge",
     fajta: "shiba inu",
     marmagassag: 40,
     neme: "kan",
@@ -34,6 +34,7 @@ const KUTYALISTA = [
 function init() {
   const ARTICLE = document.querySelector("article");
   const MAIN = document.querySelector("main");
+  const BEKULD = document.getElementById("bekuld");
   console.log(KUTYALISTA);
   let txt = osszeAllit();
   ARTICLE.innerHTML = txt;
@@ -45,6 +46,7 @@ function init() {
       torles(index);
     });
   }
+  BEKULD.addEventListener("click", bekuldes);
 
   /*GOMB.addEventListener("click", function () {
     kutya(KUTYALISTA, ARTICLE);
@@ -89,10 +91,48 @@ function tablazat() {
 }
 
 function torles(i) {
-  console.log(KUTYALISTA[i]);
-  //let gomb = event.target
-  //let szulo = gomb.parentElement
-  //szulo.remove
-  KUTYALISTA.splice(i);
-  console.log(KUTYALISTA[i]);
+  console.log(i);
+  KUTYALISTA.splice(i, 1);
+  ujragen();
+}
+
+function bekuldes() {
+  const URLAP = document.getElementById("urlap");
+  const NEV = document.getElementById("nev");
+  const FAJTA = document.getElementById("fajta");
+  const MARMAG = document.getElementById("marmagassag");
+  const KAN = document.getElementById("kan");
+  const SZUKA = document.getElementById("szuka");
+  const KOR = document.getElementById("kor");
+  const MAIN = document.querySelector("main");
+  const BEVITEL = { nev: "", fajta: "", marmagassag: 0, neme: "", kor: 0 };
+  BEVITEL["nev"] = NEV.value;
+  BEVITEL["fajta"] = FAJTA.value;
+  BEVITEL["marmagassag"] = parseInt(MARMAG.value);
+  if (KAN) {
+    BEVITEL["neme"] = "kan";
+  } else {
+    BEVITEL["neme"] = "szuka";
+  }
+  BEVITEL["kor"] = parseInt(KOR.value);
+  KUTYALISTA.push(BEVITEL);
+  console.log(KUTYALISTA);
+  ujragen();
+  //meg kell vizsgálni, hogy URLAP kulcsai egyenlőek-e a kulccsal -> pusholja a lista megfelelő elemébe
+}
+
+function ujragen() {
+  const ARTICLE = document.querySelector("article");
+  const MAIN = document.querySelector("main");
+  let txt = osszeAllit();
+  ARTICLE.innerHTML = txt;
+  txt = tablazat();
+  MAIN.innerHTML = txt;
+  const GOMB = document.getElementsByClassName("torles");
+  console.log(GOMB.length);
+  for (let index = 0; index < GOMB.length; index++) {
+    GOMB[index].addEventListener("click", function () {
+      torles(index);
+    });
+  }
 }
